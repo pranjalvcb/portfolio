18a32353d3647b4559b71cf9b1425cecc2baf785
Pranjal Singhal <107479078+pranjalvcb@users.noreply.github.com>
2023-10-28 21:19:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -67,19 +67,14 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     // Display the loader
     const loader = document.querySelector(".loader");
     loader.style.opacity = "1";
-  
+
     // Hide the loader after 2 seconds
     setTimeout(function() {
       loader.style.opacity = "0";
-  
-      // Remove the "loader" class after the opacity transition completes
-      setTimeout(function() {
-        loader.classList.remove("loader");
-      }, 1000); // 1000 milliseconds = 1 second (adjust this if needed)
+      loader.style.zIndex = "-1";
     }, 2000); // 2000 milliseconds = 2 seconds
   });
-  
